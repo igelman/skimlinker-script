e77f233f3531d7e7c7609176c513d5eaf11973aa
alan@igelman.com <Sup3rm1n>
2015-08-15 15:12:14 -0400

    Finish script to convert all rows of links.txt to skimlinks. Still need the skimlinks id, though.

--- skimlinker.php
+++ skimlinker.php
@@ -1,16 +1,28 @@
 #!/usr/bin/php
 <?php
-print "$argc arguments were passed. In order: \n";
-for ($i = 0; $i <= $argc -1; ++$i) {
-    print "$i: $argv[$i]\n";
-}
+//print "$argc arguments were passed. In order: \n";
+//for ($i = 0; $i <= $argc -1; ++$i) {
+//    print "$i: $argv[$i]\n";
+//}
+
+$skimLinksId = "123X456";
 $linksArray = file("./links.txt");
 
+echo "url | encoded url | skimlink" . PHP_EOL;
 foreach($linksArray as $link) {
-    print $link;
-    // http://go.redirectingat.com/?id=123X456&xs=1&url=http%3A%2F%2Famazon.com%2F&xcust=my_user|value5&sref=http%3A%2F%2Fyoursite.com%2Fsomepage.html
+    $url = "http://go.redirectingat.com/?id=" . $skimLinksId . "&url=" . urlencode($link);
+    curlThis($url);
+}
+    // http://go.redirectingat.com/?id=123X456&url=http%3A%2F%2Famazon.com%2F
 
-    // curl
+
+function curlThis($url) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $result = curl_exec($ch);
+    $info = curl_getinfo($ch);
+    echo $url . " | " . $info['url'] . " | " . $info['http_code'] . PHP_EOL;
+    curl_close($ch);
 }
 
 ?>
